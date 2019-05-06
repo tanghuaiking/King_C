0756f8b7fc589ea634b59c0c26ed0299ef76e8e6
唐怀金 <961881550@qq.com>
2019-05-06 17:12:36 +0800

    Add files via upload

---
+++ forcode.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int count=1;
+    for(;count <=10;++count)  //for(int count =1;count<=10;==count)
+    {
+        printf(" %d",count);
+    }
+    printf("\nAfter the loop count has the value %d.\n",count);
+    return 0;
+}
